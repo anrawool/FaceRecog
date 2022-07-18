3da40ec58894ecc7ec3decaefc7a2cecbe000271
Sarthak Abhijit Rawool <sarthakrawool09@gmail.coml>
2022-07-18 19:32:15 +0530

    Object Orientation Usuage Ready

--- DatasetGeneralizer.py
+++ DatasetGeneralizer.py
@@ -1,10 +1,19 @@
 # Code for conversion of png to jpeg
 from PIL import Image
+import shutil
+import random
 
 
 class DatasetGeneralizer():
-    def image_converter(to_convert_file, new_file, convert_file_ext, new_file_ext, start_array=1, end_array=2, ):
+    def image_converter(self, to_convert_file, new_file, convert_file_ext, new_file_ext, start_array=1, end_array=2, convert_to='RGB'):
         for i in range(start_array, end_array):
             img = Image.open(f'{to_convert_file}{i}.{convert_file_ext}')
-            rgb_img = img.convert('RGB')
+            rgb_img = img.convert(convert_to)
             rgb_img.save(f'{new_file}{i}.{new_file_ext}')
+
+    def duplicate_dataset(self, num_initial_files, expected_num_of_files, initial_file_name, copy_file_name, initial_file_extension='jpeg', copy_file_extension='jpeg'):
+        for file_no in range(num_initial_files+1, expected_num_of_files):
+            rand_no = random.randint(1, num_initial_files)
+            shutil.copy(f"{initial_file_name}{rand_no}.{copy_file_extension}",
+                        f"{copy_file_name}{file_no}.{initial_file_extension}")
+            file_no += 1
--- face_recog.py
+++ face_recog.py
@@ -4,51 +4,95 @@ import numpy as np
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.utils import to_categorical
 
-train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True)  # Parameters for Datapreprocessing
 
-training_set = train_datagen.flow_from_directory(
-    'Dataset/Training_Set/',
-    target_size=(64, 64),
-    batch_size=32,
-    class_mode='categorical')  # Training Set
+class FaceRecogTrainer():
+    pass
+    # Functions:
+    # 1. Train, Test Set Preprocesser
 
-test_datagen = ImageDataGenerator(rescale=1./255)
-test_set = test_datagen.flow_from_directory(
-    'Dataset/Test_Set/',
-    target_size=(64, 64),
-    batch_size=32,
-    class_mode='categorical')  # Test Set
+    def trainTestPP(self, Traindirectory, TestDirectory, target_size=(64, 64), batch_size=32, network_type='binary', rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True):
+        train_datagen = ImageDataGenerator(
+            rescale=rescale,
+            shear_range=shear_range,
+            zoom_range=zoom_range,
+            horizontal_flip=horizontal_flip)  # Parameters for Datapreprocessing
 
-# Building The CNN
+        self.training_set = train_datagen.flow_from_directory(
+            Traindirectory,
+            target_size=target_size,
+            batch_size=batch_size,
+            class_mode=network_type)  # Training Set
 
-cnn = tf.keras.models.Sequential()
+        test_datagen = ImageDataGenerator(rescale=1./255)
+        self.test_set = test_datagen.flow_from_directory(
+            TestDirectory,
+            target_size=target_size,
+            batch_size=batch_size,
+            class_mode=network_type)  # Test Set
+        # 2. Model Creator
 
-cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3,
-                               activation='relu', input_shape=[64, 64, 3]))
-cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
-cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
-cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
-cnn.add(tf.keras.layers.Flatten())
-cnn.add(tf.keras.layers.Dense(units=140, activation='relu'))
-cnn.add(tf.keras.layers.Dense(units=4, activation='softmax'))
+    def ModelCreator(self, activation='sigmoid', out_neurons=1, input_shape=[64, 64, 3]):
+        # Building The CNN
+        if len(self.training_set.class_indices) > 2:
+            out_neurons = len(self.training_set.class_indices)
+        else:
+            out_neurons = out_neurons
+        self.cnn = tf.keras.models.Sequential()
+
+        self.cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3,
+                                            activation='relu', input_shape=input_shape))
+        self.cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+        self.cnn.add(tf.keras.layers.Conv2D(
+            filters=32, kernel_size=3, activation='relu'))
+        self.cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+        self.cnn.add(tf.keras.layers.Flatten())
+        self.cnn.add(tf.keras.layers.Dense(units=140, activation='relu'))
+        self.cnn.add(tf.keras.layers.Dense(
+            units=out_neurons, activation=activation))
+
+    # 3. Model Compiler
+
+    def ModelCompiler(self, optim="adam", loss='binary_crossentropy', metrics=['accuracy']):
+        # # Compiling the CNN
+        self.cnn.compile(optimizer=optim, loss=loss,
+                         metrics=metrics)
+
+    # 4. Model Trainer
+
+    def ModelTrainer(self, epochs=5):
+        self.cnn.fit(x=self.training_set,
+                     validation_data=self.test_set, epochs=epochs)
+
+    # 5. Model Saver
+
+    def ModelSaver(self, saveDirectory):
+        self.cnn.save(f'{saveDirectory}/model')
+
+    def TestImagePred(self, pred_img_path, model_path, target_size=(64, 64)):
+        cnn = tf.keras.models.load_model(model_path)
+        Testimage = image.load_img(
+            pred_img_path, target_size=target_size)
+        Testimage = image.img_to_array(Testimage)
+        Testimage = np.expand_dims(Testimage, axis=0)
+        result = cnn.predict(Testimage/255.0)
+        return result
 
 
-# # Compiling the CNN
+# Example Usuage
 
-cnn.compile(optimizer='adam', loss='categorical_crossentropy',
-            metrics=['accuracy'])
+FaceRecog = FaceRecogTrainer()
+FaceRecog.trainTestPP('./Dataset/Training_Set',
+                      './Dataset/Test_Set', network_type='categorical')
+FaceRecog.ModelCreator(activation='softmax')
+FaceRecog.ModelCompiler(loss='categorical_crossentropy')
+FaceRecog.ModelTrainer(epochs=1)
+FaceRecog.ModelSaver(saveDirectory="test_model")
+result = FaceRecog.TestImagePred('single_pred/image.jpeg',
+                                 'saved_model/my_model', target_size=(64, 64))
 
-cnn.fit(x=training_set, validation_data=test_set, epochs=8)
+results = []
 
-test_image = image.load_img(
-    'single_pred/ab.jpeg', target_size=(64, 64))
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis=0)
-result = cnn.predict(test_image/255.0)
-
-
-cnn.save('saved_model/my_model')
+for i in result[0]:
+    x = round(i, 2)
+    results.append(x)
+print(results)
